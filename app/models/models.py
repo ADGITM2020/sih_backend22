f27856d5867ef36dfa42a3440caad959dd889d69
kunalbhatia-868 <kunalbhatia868@gmail.com>
2022-03-28 11:30:03 +0530

    Student endpoints completed

--- app/models/models.py
+++ app/models/models.py
@@ -6,19 +6,13 @@ from sqlalchemy.orm import relationship
 class Student(Base):
     __tablename__ = 'student'
 
-    def __init__(self, name: str, email: str, password: str):
-        self.name = name
-        self.email = email
-        self.password = password
-
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String)
     # institute_id = Column(Integer, ForeignKey("institute.institute_id"))
     password = Column(String)
-    year = Column(String)
+    year = Column(Integer)
     course = Column(String)
-    college_id = Column(String)
     is_student = Column(Boolean)
 
     # slots = relationship("Slot", back_populates="student")
